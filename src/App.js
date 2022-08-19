04b1e2287d345d555c0cca6e80b99d492afdafd3
IlhamisAymanila <74014773+IlhamisAymanila@users.noreply.github.com>
2022-08-19 20:57:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Dynamicform from './form';
+
+export default function App() {
+  return (
+    <div className="App m-5">
+      <h1 className='mb-5'>Dynamic Form</h1>
+      <Dynamicform/>
+    </div>
+  );
+}
